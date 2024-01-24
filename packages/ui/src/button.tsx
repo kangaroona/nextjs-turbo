941e7518ac057f49f8eb62cf19b16c73cdc54b58
kangaroona <79397550@qq.com>
2024-01-24 00:06:00 -0800

    feat: add tanstackquery as client request lib

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { ReactNode } from "react";
-
+import {
+  useQuery,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 export interface ButtonProps {
   children: ReactNode;
   className?: string;
@@ -9,6 +13,16 @@ export interface ButtonProps {
 }
 
 export const Button = ({ children, className, appName }: ButtonProps) => {
+  function getData(): Promise<number> {
+    const data = 5;
+    return Promise.resolve(data);
+  }
+  const { data, isError } = useQuery({
+    //    ^? const data: number | undefined
+    queryKey: ["test"],
+    queryFn: getData,
+  });
+  console.log("test data", data);
   return (
     <button
       className={className}
